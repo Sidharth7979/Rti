fdd34b2320bd080fe3d4d7256abc2eb7b90fa136
Sidharth7979 <sidhubhai1997@gmail.com>
2024-05-04 18:38:31 +0530

    Rti added

---
+++ src/screens/Rti.js
@@ -0,0 +1,79 @@
+import React from 'react'
+
+export default function Rti() {
+  return (
+    <div>
+    <div className='Rti'>
+       <h1>RTI <br/>
+       Right To Information</h1>
+      </div>
+<div className='first'>
+<h3>RIGHT TO INFORMATION</h3>
+<hr></hr>
+<h4>Information Sought Under the Act Would Be Scrutinized and Considered if the Rti Application Received by Chandigarh University Fulfills the Criteria for Providing the Information Sought and Is in Accordance With the Provisions Stipulated in the Act.</h4>
+
+<h3>Names, Designations and other particulars of the Public Information Officers :</h3>
+</div>
+<div>
+<table className='tablefirst'>
+  <thead>
+    <tr>
+      <th>
+S. No.</th>
+      <th>Name</th>
+      <th>Designation</th>
+      <th>Position</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Navjyot Singh Dhadli</td>
+      <td>Assistant Registrar</td>
+      <td>Public Information Officer<br/>
+Chandigarh University, Gharuan,<br/>
+Dist. SAS Nagar (Punjab)<br/>
+Email: navjot.e16860@cumail.in</td>
+      </tr>
+     <tr>
+   <td>2</td>
+   <td>Dr. Gurdarshan Singh Brar</td>
+   <td>	Registrar</td>
+   <td>Appellate Authority</td>
+     </tr>
+      
+  </tbody>
+
+
+  
+</table>
+<hr className='ttt'></hr>
+</div>
+
+<div className='lastbottom'>
+<h3>Section 4(1) (b) (i), The Particulars of its Organization, Functions and Duties :</h3>
+<p>Chandigarh University, Gharuan(Punjab) was established under the Chandigarh University Act, 2012, enacted by the Government of Punjab and recognized by the Government of India Under the provisions of Section 2(f) of the University Grants Commission Act, 1956.</p>
+
+<h3>Authorities of the University :</h3>
+<p>The following are authorities of the University, namely –</p>
+<ul>
+  <li>The Governing Body;</li>
+  <li>The Board of Management;</li>
+  <li>The Academic Council; and</li>
+  <li>Such other authorities, as may be declared by the Statutes to be the authorities of the University.</li>
+</ul>
+
+</div>
+<hr className='ttt'></hr>
+       </div>
+
+)
+}
+
+    
+
+       
+  
+    
+
+    
---
+++ src/screens/tab2.js
@@ -0,0 +1,52 @@
+import React from 'react'
+
+export default function tab2() {
+  return (
+    
+    <div className='gbody'>
+
+<h3>The Governing Body :</h3>
+
+<table className='tabletwo'>
+    <tbody>
+      <tr>
+        <td>a)</td>
+        <td>The Chancellor</td>
+        <td>Chairman</td>
+      </tr>
+      <tr>
+        <td>b)</td>
+        <td>The Vice Chancellor</td>
+        <td>Member</td>
+      </tr>
+      <tr>
+        <td>c)</td>
+        <td>Three persons nominated by the Trust, out of whom two shall be eminent educationist;</td>
+        <td>Member</td>
+      </tr>
+      <tr>
+        <td>d)</td>
+        <td>One expert member of management or information technology</td>
+        <td>Member</td>
+      </tr>
+      <tr>
+        <td>e)</td>
+        <td>One expert of finance, nominated by the Chancellor</td>
+        <td>Member</td>
+      </tr>
+      <tr>
+        <td>f)</td>
+        <td>The Secretary to Government of Punjab, Department of Higher Education or his representative not below the rank of joint secretary</td>
+        <td>Member</td>
+      </tr>
+      <tr>
+        <td>g)</td>
+        <td>One eminent educationist nominated by the Secretary to Government of Punjab, Department of Higher Education in consultation with the Chancellor</td>
+        <td>Member</td>
+      </tr>
+    </tbody>
+</table>
+<hr></hr>
+    </div>
+  )
+}
